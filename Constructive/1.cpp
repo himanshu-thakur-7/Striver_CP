35a4c80916beae318331092e66c5ca0bbfe9f3f7
himanshu-thakur-7 <himan7781@gmail.com>
2023-09-13 20:42:34 +0530

    solved constructive problem 1

---
+++ Constructive/1.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int x = 0;
+    while (n--)
+    {
+        string stmt;
+        cin >> stmt;
+
+        if (stmt[0] == '+' || stmt[2] == '+')
+        {
+            x++;
+        }
+        else
+        {
+            x--;
+        }
+    }
+    cout << x << endl;
+}
