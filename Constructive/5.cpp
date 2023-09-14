4318a1240940bcfab0c60b1761f6b6e347a12e74
himanshu-thakur-7 <himan7781@gmail.com>
2023-09-14 21:06:44 +0530

    solved constructive problem 5

---
+++ Constructive/5.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x1, x2, x3;
+    cin >> x1 >> x2 >> x3;
+
+    int minDist = INT_MAX;
+    for (int meetingPoint = 1; meetingPoint <= 100; meetingPoint++)
+    {
+        int distTrav = abs(x1 - meetingPoint) + abs(x2 - meetingPoint) + abs(x3 - meetingPoint);
+        minDist = min(distTrav, minDist);
+    }
+
+    cout << minDist << endl;
+
+    return 0;
+}
