22efc10a5f97b96d8463a6e877730d7b6fbac794
himanshu-thakur-7 <himan7781@gmail.com>
2023-09-18 23:07:42 +0530

    solved constructive problem 10

---
+++ Constructive/10.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    string ans = "EASY";
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+
+        if (val == 1)
+        {
+            ans = "HARD";
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
